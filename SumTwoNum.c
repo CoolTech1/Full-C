e282d05037f2029d17aad5dad1d319d460ddc6e4
Varun Tyagi <148223168+CoolTech1@users.noreply.github.com>
2023-10-31 21:35:10 +0530

    Add files via upload

---
+++ SumTwoNum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("Enter 1st number \n");
+	scanf("%d",&a);
+	printf("Enter 2nd number \n");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("Sum is %d",sum);
+	return 0;
+}
